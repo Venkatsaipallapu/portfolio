4b5b24da986c27f4afb9a4fb8396e8c06a181afa
Venkat Sai Pallapu <venkatsaipallapu@Mac.attlocal.net>
2025-03-23 13:53:56 -0400

    modules change

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/portfolio/", // ✅ Set the base path for GitHub Pages
   server: {
-    host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && false, 
   ].filter(Boolean),
   resolve: {
     alias: {
